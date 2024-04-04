73d9633cf701c497034a32ba71f18cabf6be1278
Miguel de Icaza <miguel@gnome.org>
2024-04-03 22:57:51 -0400

    Add a hook to track changes

--- Sources/RunestoneUI/TextViewUI.swift
+++ Sources/RunestoneUI/TextViewUI.swift
@@ -2,15 +2,22 @@
 // https://docs.swift.org/swift-book
 
 import SwiftUI
+
 import UIKit
 @_exported import Runestone
 
 public struct TextViewUI: UIViewRepresentable {
-    @Binding var text: String
     @Environment(\.language) var language: TreeSitterLanguage?
-    
-    public init (text: Binding<String>) {
+    @Binding var text: String
+    let onChange: (_ content: String, _ location: CGRect?, _ selectionRange: (TextLocation,TextLocation))->()
+
+    /// Creates a TextViewUI with the contents of the specified string, and it will invoke the onChange method when changes to it happen
+    /// - Parameters:
+    ///  - text: The text to edit
+    ///  - onChange: callback that is invoked when the text changes, it includes the complete text, the location in the screen where the cursor sits, and the selection range (the last componet is the location of the cursor)
+    public init (text: Binding<String>, onChange: @escaping (_ content: String, _ location: CGRect?, _ selectionRange: (TextLocation,TextLocation))->()) {
         self._text = text
+        self.onChange = onChange
     }
     
     public func makeUIView(context: Context) -> TextView {
@@ -40,7 +47,7 @@ public struct TextViewUI: UIViewRepresentable {
     }
  
     public func makeCoordinator() -> TextViewCoordinator {
-        return TextViewCoordinator(text: $text)
+        return TextViewCoordinator(text: $text, onChange: onChange)
     }
     
     public func updateUIView(_ uiView: Runestone.TextView, context: Context) {
@@ -57,17 +64,40 @@ public struct TextViewUI: UIViewRepresentable {
     public class TextViewCoordinator: TextViewDelegate {
         var language: TreeSitterLanguage? = nil
         var text: Binding<String>
-        
-        init (text: Binding<String>) {
+        let onChange: (_ content: String, _ location: CGRect?, _ selectionRange: (TextLocation,TextLocation))->()
+
+        init (text: Binding<String>, onChange: @escaping (_ content: String, _ location: CGRect?, _ selectionRange: (TextLocation,TextLocation))->()) {
             self.text = text
+            self.onChange = onChange
         }
         
         public func textViewDidChange(_ textView: TextView) {
-            text.wrappedValue = textView.text
+            let newText = textView.text
+            text.wrappedValue = newText
+            var region: CGRect? = nil
+            
+            if let r = textView.selectedTextRange {
+                region = textView.firstRect(for: r)
+            }
+            let range = textView.selectedRange
+            let start = textView.textLocation(at: range.location)
+            let end = textView.textLocation(at: range.location + range.length)
+            guard let start, let end else {
+                // If this happened, something very wrong went on
+                print ("Start and end were not resolved out of the \(range) returned by TextView on textViewChange")
+                return
+            }
+            onChange (newText, region, (start, end))
+        }
+        
+        public func textView(_ textView: TextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            print ("Range is: loc: \(range.location) len: \(range.length)")
+            
+            return true
         }
     }
 }
-
+ 
 public struct LanguageKey : EnvironmentKey {
     public static let defaultValue: TreeSitterLanguage? = nil
 }
